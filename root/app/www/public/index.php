fdc85a7eddfb473f3b514a762f2e3544dc9cd975
Austin Best <austin.w.best@gmail.com>
2023-12-11 21:34:36 -0500

    Add some instructions on syno docker perms

--- root/app/www/public/index.php
+++ root/app/www/public/index.php
@@ -35,12 +35,25 @@ if ($apiError) {
     <div id="content-commands" style="display: none;"></div>
     <div id="content-dockerPermissions" style="display: <?= ($dockerPerms ? 'none' : 'block') ?>;">
         If you are seeing this, it means the user:group running this container does not have permission to run docker commands. Please fix that, restart the container and try again.<br><br>
-        An example for Ubuntu:
-        <div class="ms-2">
-            Set the PGID to the docker group (998 or 999 but you can run <code>grep docker /etc/group</code>) and then:<br>
-            Change the user:group with a chown<br>
-            Wipe the appdir and retry<br>
-            Try with --force-recreate
+        <div class="bg-secondary rounded p-4">
+            An example for Ubuntu:
+            <div class="ms-2">
+                Set the PGID to the docker group<br>
+                &nbsp;&nbsp;&nbsp;- <code>ls -ltra /var/run/docker.sock</code> to see the group running the sock ("docker" for example)<br>
+                &nbsp;&nbsp;&nbsp;- <code>grep docker /etc/group</code> to see the group id and use as the PGID<br>
+                Change the user:group with a chown (if necessary)<br>
+                Wipe the appdir and retry (if necessary)<br>
+                Try with --force-recreate (if necessary)
+            </div>
+        </div>
+        <div class="bg-secondary rounded p-4 mt-3">
+            An example for Synology:
+            <div class="ms-2">
+                Create a docker group <code>sudo synogroup --add docker</code><br>
+                &nbsp;&nbsp;&nbsp;- Take note of the group id it returns next to Group Id: [65537] (for example)<br>
+                Adjust docker sock permissions: <code>sudo chown root:docker /var/run/docker.sock</code><br>
+                Assign the PGID to the group id from above and restart (docker-compose up -d)
+            </div>
         </div>
     </div>
 <?php
