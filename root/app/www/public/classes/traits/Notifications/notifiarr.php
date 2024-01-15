029160a9541ec6ac0bedb178dc74c284cd60ec8c
Austin Best <austin.w.best@gmail.com>
2024-01-14 19:12:40 -0500

    Renaming shuffle...

---
+++ root/app/www/public/classes/traits/Notifications/notifiarr.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+----------------------------------
+ ------  Created: 111623   ------
+ ------  Austin Best	   ------
+----------------------------------
+*/
+
+trait Notifiarr
+{
+    public function notifiarr($logfile, $payload)
+    {
+        $headers    = ['x-api-key:' . $this->platformSettings[1]['apikey']];
+        $url        = 'https://notifiarr.com/api/v1/notification/dockwatch';
+        $curl       = curl($url, $headers, 'POST', json_encode($payload));
+
+        logger($logfile, 'notification response:' . json_encode($curl), ($curl['code'] != 200 ? 'error' : ''));
+
+        $return = ['code' => 200];
+        if ($curl['code'] != 200) {
+            logger($logfile, 'sending a retry in 5s...');
+            sleep(5);
+
+            $curl = curl($url, $headers, 'POST', json_encode($payload));
+            logger($logfile, 'notification response:' . json_encode($curl), ($curl['code'] != 200 ? 'error' : ''));
+
+            if ($curl['code'] != 200) {
+                $return = ['code' => $curl['code'], 'error' => $curl['response']['details']['response']];
+            }
+        }
+
+        return $return;
+    }
+}
