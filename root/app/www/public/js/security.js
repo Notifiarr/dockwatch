1a458e4e0895d8d5b8baa698a087d759fe8d32c3
nzxl101 <yyy.noel.yyy@gmail.com>
2026-04-27 03:20:32 +0200

    fix(mysql): remove ping loop

--- root/app/www/public/js/security.js
+++ root/app/www/public/js/security.js
@@ -274,7 +274,7 @@ function viewSecurityScanByFile(hash, image, file) {
                         html += '<tr>';
 
                         if (idx === 0) {
-                            html += '<td class="bg-secondary" rowspan="' + rowspan + '">' + pkgName.slice(0, 32) + "..." + '</td>';
+                            html += '<td class="bg-secondary" rowspan="' + rowspan + '">' + (pkgName.length > 14 ? pkgName.slice(0, 14) + "..." : pkgName) + '</td>';
                         }
 
                         html += `
